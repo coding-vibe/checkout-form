3c5d19711f0f0707c03ee6ff59c2a7cbc10b0a5e
merry <k.maria.m.24.05@gmail.com>
2023-10-22 00:00:51 +0300

    add screen

--- src/pages/DeliveryModeScreen/DeliveryModeScreen.tsx
+++ src/pages/DeliveryModeScreen/DeliveryModeScreen.tsx
@@ -1,1 +1,40 @@
-export default function DeliveryModeScreen() {}
+import { useContext } from 'react';
+import { Form } from 'react-final-form';
+import Button from '@mui/material/Button';
+import MenuItem from '@mui/material/MenuItem';
+import { Select } from 'mui-rff';
+import FormScreens from 'constants/formScreens';
+import WizardFormContext from 'contexts/WizardFormContext';
+import { validateIsRequired } from 'utils/validation';
+
+type DeliveryMode = { deliveryMode: null };
+
+export default function DeliveryModeScreen() {
+  const { onSaveFormValues } = useContext(WizardFormContext);
+
+  return (
+    <Form<DeliveryMode>
+      onSubmit={(values) => {
+        onSaveFormValues(FormScreens.DELIVERY_MODE, values);
+      }}
+      render={({ handleSubmit }) => (
+        <form onSubmit={handleSubmit}>
+          <Select
+            fieldProps={{ validate: validateIsRequired }}
+            label='Delivery Type'
+            name='deliveryType'
+            placeholder='Choose delivery type'
+            sx={{ mb: 2 }}>
+            <MenuItem value='postOffice'>Post Office Delivery</MenuItem>
+            <MenuItem value='courier'>Courier Delivery</MenuItem>
+          </Select>
+          <Button
+            type='submit'
+            variant='contained'>
+            Next step
+          </Button>
+        </form>
+      )}
+    />
+  );
+}
