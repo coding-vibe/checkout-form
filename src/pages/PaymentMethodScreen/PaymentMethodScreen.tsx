a3a08080f35bdd8d7871265e4ca22a72c93cbdb8
merry <k.maria.m.24.05@gmail.com>
2023-10-22 21:42:49 +0300

    fixes

--- src/pages/PaymentMethodScreen/PaymentMethodScreen.tsx
+++ src/pages/PaymentMethodScreen/PaymentMethodScreen.tsx
@@ -1,19 +1,25 @@
 import { useContext } from 'react';
 import { Form } from 'react-final-form';
 import Button from '@mui/material/Button';
-import { MenuItem } from '@mui/material';
+import MenuItem from '@mui/material/MenuItem';
 import { Select } from 'mui-rff';
 import FormScreens from 'constants/formScreens';
-import WizardFormContext from 'contexts/WizardFormContext';
+import WizardFormContext, {
+  initialFormValues,
+} from 'contexts/WizardFormContext';
 import { validateIsRequired } from 'utils/validation';
 
-type PaymentMethod = { paymentMethod: null };
+type PaymentMethodType = (typeof initialFormValues)[FormScreens.PAYMENT_METHOD];
 
 export default function PaymentMethodScreen() {
   const { onSaveFormValues } = useContext(WizardFormContext);
+  const CASH_PAYMENT_OPTION = 'Cash';
+  const CARD_PAYMENT_OPTION = 'Credit Card';
+  const CASH_PAYMENT_VALUE = 'cash';
+  const CARD_PAYMENT_VALUE = 'creditCard';
 
   return (
-    <Form<PaymentMethod>
+    <Form<PaymentMethodType>
       onSubmit={(values) => {
         onSaveFormValues(FormScreens.PAYMENT_METHOD, values);
       }}
@@ -22,12 +28,16 @@ export default function PaymentMethodScreen() {
           <Select
             fieldProps={{ validate: validateIsRequired }}
             label='Payment Method'
-            name='paymentMethod'>
-            <MenuItem value='cash'>Cash</MenuItem>
-            <MenuItem value='creditCard'>Credit Card</MenuItem>
+            name='paymentMethod'
+            sx={{ mb: 2 }}>
+            <MenuItem value={CASH_PAYMENT_VALUE}>
+              {CASH_PAYMENT_OPTION}
+            </MenuItem>
+            <MenuItem value={CARD_PAYMENT_VALUE}>
+              {CARD_PAYMENT_OPTION}
+            </MenuItem>
           </Select>
           <Button
-            sx={{ mt: 2 }}
             type='submit'
             variant='contained'>
             Next step
