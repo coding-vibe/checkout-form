62e271adcdb620becad92c9bf96358e23812ed08
merry <k.maria.m.24.05@gmail.com>
2023-10-22 22:00:02 +0300

    fixes

--- src/pages/DeliveryModeScreen/DeliveryModeScreen.tsx
+++ src/pages/DeliveryModeScreen/DeliveryModeScreen.tsx
@@ -4,10 +4,21 @@ import Button from '@mui/material/Button';
 import MenuItem from '@mui/material/MenuItem';
 import { Select } from 'mui-rff';
 import FormScreens from 'constants/formScreens';
-import WizardFormContext from 'contexts/WizardFormContext';
+import WizardFormContext, {
+  initialFormValues,
+} from 'contexts/WizardFormContext';
 import { validateIsRequired } from 'utils/validation';
 
-type DeliveryMode = { deliveryMode: null };
+const DELIVERY_MODE_OPTIONS = {
+  postOffice: 'Post Office Delivery',
+  courier: 'Courier Delivery',
+};
+const DELIVERY_MODE_VALUES = {
+  postOffice: 'postOffice',
+  courier: 'courier',
+};
+
+type DeliveryMode = (typeof initialFormValues)[FormScreens.DELIVERY_MODE];
 
 export default function DeliveryModeScreen() {
   const { onSaveFormValues } = useContext(WizardFormContext);
@@ -22,12 +33,16 @@ export default function DeliveryModeScreen() {
           <Select
             fieldProps={{ validate: validateIsRequired }}
             label='Delivery Type'
-            name='deliveryType'>
-            <MenuItem value='postOffice'>Post Office Delivery</MenuItem>
-            <MenuItem value='courier'>Courier Delivery</MenuItem>
+            name='deliveryType'
+            sx={{ mb: 2 }}>
+            <MenuItem value={DELIVERY_MODE_VALUES.postOffice}>
+              {DELIVERY_MODE_OPTIONS.postOffice}
+            </MenuItem>
+            <MenuItem value={DELIVERY_MODE_VALUES.courier}>
+              {DELIVERY_MODE_OPTIONS.courier}
+            </MenuItem>
           </Select>
           <Button
-            sx={{ mt: 2 }}
             type='submit'
             variant='contained'>
             Next step
