943b470840b9183de76fe3c1f5b0ccf9e1640556
merry <k.maria.m.24.05@gmail.com>
2023-10-27 13:01:08 +0300

    add styles

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,18 @@
 import { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import { StyledEngineProvider } from '@mui/material/styles';
 import Routes from 'components/Routes';
 import WizardFormProvider from 'components/WizardFormProvider';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
-      <WizardFormProvider>
-        <Routes />
-      </WizardFormProvider>
+      <StyledEngineProvider injectFirst>
+        <WizardFormProvider>
+          <Routes />
+        </WizardFormProvider>
+      </StyledEngineProvider>
     </BrowserRouter>
   </StrictMode>,
 );
--- src/pages/CreditCardDetailsScreen/CreditCardDetailsScreen.tsx
+++ src/pages/CreditCardDetailsScreen/CreditCardDetailsScreen.tsx
@@ -14,6 +14,7 @@ import {
   validateIsFutureDate,
   validateIsRequired,
 } from 'utils/validation';
+import * as classes from './styles';
 
 const CARD_NUMBER_LENGTH = 16;
 const CVV_CODE_LENGTH = 3;
@@ -25,53 +26,55 @@ export default function CreditCardDetailsScreen() {
   const { onSaveFormValues } = useContext(WizardFormContext);
 
   return (
-    <Form<CreditCardDetailsType>
-      onSubmit={(values) => {
-        onSaveFormValues(FormScreens.CREDIT_CARD_DETAILS, values);
-      }}
-      render={({ handleSubmit }) => (
-        <form onSubmit={handleSubmit}>
-          <CardNumberField
-            fieldProps={{
-              validate: composeValidators(
-                validateIsRequired,
-                validateDigitsCount(CARD_NUMBER_LENGTH, 'credit card number'),
-              ),
-            }}
-            label='Card Number'
-            name='cardNumber'
-            sx={{ mb: 2 }}
-          />
-          <CVVCodeField
-            fieldProps={{
-              validate: composeValidators(
-                validateIsRequired,
-                validateDigitsCount(CVV_CODE_LENGTH, 'CVV code'),
-              ),
-            }}
-            label='CVV Code'
-            name='cvvCode'
-            placeholder='Enter CVV code'
-            sx={{ mb: 2 }}
-          />
-          <CardExpiryField
-            fieldProps={{
-              validate: composeValidators(
-                validateIsRequired,
-                validateIsFutureDate,
-              ),
-            }}
-            label='Expiry Date'
-            name='expiryDate'
-            sx={{ mb: 2 }}
-          />
-          <Button
-            type='submit'
-            variant='contained'>
-            Next step
-          </Button>
-        </form>
-      )}
-    />
+    <div css={classes.wrap}>
+      <Form<CreditCardDetailsType>
+        onSubmit={(values) => {
+          onSaveFormValues(FormScreens.CREDIT_CARD_DETAILS, values);
+        }}
+        render={({ handleSubmit }) => (
+          <form onSubmit={handleSubmit}>
+            <CardNumberField
+              fieldProps={{
+                validate: composeValidators(
+                  validateIsRequired,
+                  validateDigitsCount(CARD_NUMBER_LENGTH, 'credit card number'),
+                ),
+              }}
+              label='Card Number'
+              name='cardNumber'
+              sx={{ mb: 2 }}
+            />
+            <CVVCodeField
+              fieldProps={{
+                validate: composeValidators(
+                  validateIsRequired,
+                  validateDigitsCount(CVV_CODE_LENGTH, 'CVV code'),
+                ),
+              }}
+              label='CVV Code'
+              name='cvvCode'
+              placeholder='Enter CVV code'
+              sx={{ mb: 2 }}
+            />
+            <CardExpiryField
+              fieldProps={{
+                validate: composeValidators(
+                  validateIsRequired,
+                  validateIsFutureDate,
+                ),
+              }}
+              label='Expiry Date'
+              name='expiryDate'
+              sx={{ mb: 2 }}
+            />
+            <Button
+              type='submit'
+              variant='contained'>
+              Next step
+            </Button>
+          </form>
+        )}
+      />
+    </div>
   );
 }
