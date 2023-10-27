92316ff6e1671b975b0a0179c2d335e340752ca5
merry <k.maria.m.24.05@gmail.com>
2023-10-27 19:37:58 +0300

    fix padding

--- src/pages/CreditCardDetailsScreen/styles.ts
+++ src/pages/CreditCardDetailsScreen/styles.ts
@@ -1,11 +1,11 @@
 import { css } from '@emotion/react';
 
 export const form = css`
-  margin-bottom: 20px;
-  padding: 80px;
-  border-radius: 20px;
   width: 450px;
   height: 150px;
+  margin-bottom: 20px;
+  padding: 40px;
+  border-radius: 20px;
   background-color: #8ae9b3;
   background-image: linear-gradient(315deg, #8ae9b3 0%, #c8d6e5 74%);
   box-shadow: 15px 10px 10px rgb(85, 87, 96, 0.1);
