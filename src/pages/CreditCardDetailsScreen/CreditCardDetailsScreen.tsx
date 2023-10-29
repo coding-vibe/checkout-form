21fe0d8c3df4ede32e232c5cce71a12db49ad553
merry <54949862+coding-vibe@users.noreply.github.com>
2023-10-29 17:24:32 +0200

    Credit card details screen styles (#24)
    * add styles
    
    * add config
    
    * add styles
    
    * add gradient
    
    * fix padding
    
    * change name of class
    
    * remove height

--- src/pages/CreditCardDetailsScreen/CreditCardDetailsScreen.tsx
+++ src/pages/CreditCardDetailsScreen/CreditCardDetailsScreen.tsx
@@ -14,6 +14,7 @@ import {
   validateIsFutureDate,
   validateIsRequired,
 } from 'utils/validation';
+import * as classes from './styles';
 
 const CARD_NUMBER_LENGTH = 16;
 const CVV_CODE_LENGTH = 3;
@@ -31,41 +32,47 @@ export default function CreditCardDetailsScreen() {
       }}
       render={({ handleSubmit }) => (
         <form onSubmit={handleSubmit}>
-          <CardNumberField
-            fieldProps={{
-              validate: composeValidators(
-                validateIsRequired,
-                validateDigitsCount(CARD_NUMBER_LENGTH, 'credit card number'),
-              ),
-            }}
-            label='Card Number'
-            name='cardNumber'
-            sx={{ mb: 2 }}
-          />
-          <CVVCodeField
-            fieldProps={{
-              validate: composeValidators(
-                validateIsRequired,
-                validateDigitsCount(CVV_CODE_LENGTH, 'CVV code'),
-              ),
-            }}
-            label='CVV Code'
-            name='cvvCode'
-            placeholder='Enter CVV code'
-            sx={{ mb: 2 }}
-          />
-          <CardExpiryField
-            fieldProps={{
-              validate: composeValidators(
-                validateIsRequired,
-                validateIsFutureDate,
-              ),
-            }}
-            label='Expiry Date'
-            name='expiryDate'
-            sx={{ mb: 2 }}
-          />
+          <div css={classes.fields}>
+            <CardNumberField
+              css={classes.field}
+              fieldProps={{
+                validate: composeValidators(
+                  validateIsRequired,
+                  validateDigitsCount(CARD_NUMBER_LENGTH, 'credit card number'),
+                ),
+              }}
+              label='Card Number'
+              name='cardNumber'
+              sx={{ mb: 5 }}
+            />
+            <div css={classes.wrap}>
+              <CardExpiryField
+                css={classes.field}
+                fieldProps={{
+                  validate: composeValidators(
+                    validateIsRequired,
+                    validateIsFutureDate,
+                  ),
+                }}
+                label='Expiry Date'
+                name='expiryDate'
+              />
+              <CVVCodeField
+                css={classes.field}
+                fieldProps={{
+                  validate: composeValidators(
+                    validateIsRequired,
+                    validateDigitsCount(CVV_CODE_LENGTH, 'CVV code'),
+                  ),
+                }}
+                label='CVV Code'
+                name='cvvCode'
+                placeholder='Enter CVV code'
+              />
+            </div>
+          </div>
           <Button
+            sx={{ ml: 1 }}
             type='submit'
             variant='contained'>
             Next step
---
+++ src/pages/CreditCardDetailsScreen/styles.ts
@@ -0,0 +1,22 @@
+import { css } from '@emotion/react';
+
+export const fields = css`
+  width: 450px;
+  margin-bottom: 20px;
+  padding: 40px;
+  border-radius: 20px;
+  background-color: #8ae9b3;
+  background-image: linear-gradient(315deg, #8ae9b3 0%, #c8d6e5 74%);
+  box-shadow: 15px 10px 10px rgb(85, 87, 96, 0.1);
+`;
+
+export const wrap = css`
+  display: flex;
+  gap: 75px;
+`;
+
+export const field = css`
+  & .MuiInputBase-colorPrimary {
+    border-radius: 10px;
+  }
+`;
