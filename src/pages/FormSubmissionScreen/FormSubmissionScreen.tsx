3f74d8cb424677957ad4fcdd8964c406a91e3e6b
merry <k.maria.m.24.05@gmail.com>
2023-11-06 21:44:22 +0200

    add main part of screen

--- src/pages/FormSubmissionScreen/FormSubmissionScreen.tsx
+++ src/pages/FormSubmissionScreen/FormSubmissionScreen.tsx
@@ -1,25 +1,53 @@
 import { useContext } from 'react';
+import { Checkboxes } from 'mui-rff';
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+import Button from '@mui/material/Button';
+import Link from '@mui/material/Link';
+import Paper from '@mui/material/Paper';
+import Tooltip from '@mui/material/Tooltip';
 import WizardFormContext from 'contexts/WizardFormContext';
+import routes from 'constants/routes';
+import * as classes from './styles';
 
 export default function FormSubmissionScreen() {
   const { formValues } = useContext(WizardFormContext);
 
   return (
     <div>
+      <h1 css={classes.mainTitle}>Verify recorded form data</h1>
       {Object.entries(formValues).map(([screenName, screenInfo], idx) => (
-        // eslint-disable-next-line react/no-array-index-key
-        <div key={idx}>
-          <div>{screenName}</div>
-          <div>
-            {screenInfo &&
-              Object.entries(screenInfo).map(([fieldName, fieldValue]) => (
-                <div>
-                  {fieldName}:{fieldValue}
-                </div>
-              ))}
+        <Paper
+          sx={{ p: 1, mb: 1 }}
+          // eslint-disable-next-line react/no-array-index-key
+          key={idx}>
+          <div css={classes.titleWrap}>
+            <h2 css={classes.title}>{screenName}&nbsp;</h2>
+            <Tooltip title='Click to return to the previous section'>
+              <Link
+                // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+                // @ts-ignore
+                // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+                href={routes[screenName]}
+                underline='hover'>
+                <ArrowBackIcon />
+              </Link>
+            </Tooltip>
           </div>
-        </div>
+          {screenInfo &&
+            Object.entries(screenInfo).map(([fieldName, fieldValue]) => (
+              <div css={classes.wrap}>
+                <span>{fieldName}:&nbsp;</span>
+                <span>{fieldValue}</span>
+              </div>
+            ))}
+        </Paper>
       ))}
+      <Checkboxes></Checkboxes>
+      <Button
+        type='submit'
+        variant='contained'>
+        Next step
+      </Button>
     </div>
   );
 }
---
+++ src/pages/FormSubmissionScreen/styles.ts
@@ -0,0 +1,22 @@
+import { css } from '@emotion/react';
+
+export const mainTitle = css`
+  margin: 0px 0px 16px;
+  font-size: 22px;
+  text-transform: capitalize;
+`;
+
+export const title = css`
+  font-size: 16px;
+  text-transform: capitalize;
+`;
+
+export const titleWrap = css`
+  display: flex;
+  align-items: center;
+`;
+
+export const wrap = css`
+  display: flex;
+  align-items: center;
+`;
